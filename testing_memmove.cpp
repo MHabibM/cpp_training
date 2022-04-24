1dad9636318c1c30fc96283fda4c45f44b4e7711
Mohamed Habib Mallek <53487675+MHabibM@users.noreply.github.com>
2022-04-24 03:33:04 +0100

    Update testing_memmove.cpp

--- testing_memmove.cpp
+++ testing_memmove.cpp
@@ -7,7 +7,6 @@ int main()
 {
 	int src[] = {1, 2, 3, 4};
 	int *dest=(int*)malloc(sizeof(src));
-	memset(dest, '\0', 5);
 	memmove(dest, src, sizeof(src));
 	cout << "sizeof src = " << sizeof(src) << " | sizeof dest = " << sizeof(dest) << " | sizeof int = " << sizeof(int) << endl;
 	for (int i = 0; i < (sizeof(src)/sizeof(int)); i++){
